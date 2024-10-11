d9305d03541eb58728c3dd6530a90df2b459aef4
emad <emadrar15@gmail.com>
2024-10-11 15:28:56 +0300

    Update v7

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is my simple web app."
+    return "Hello, World! This is my simple web app. Second run "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
