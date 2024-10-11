4b3de58df527715ee71130f331a73c78c52c49e4
emad <emadrar15@gmail.com>
2024-10-11 15:18:38 +0300

    updated v4

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-app = Flask(__depi__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, World! This is my simple web app."
 
-if __depi__ == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
