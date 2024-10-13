b4414de36e23f53975b751f2a9491e23eb260e94
emad <emadrar15@gmail.com>
2024-10-13 21:29:13 +0300

    test second

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get('/')
-    assert response.data == b'Hello, World! This is my simple web app. secon test'
+    assert response.data == b'Hello, World! This is my simple web app.secon test'
 
